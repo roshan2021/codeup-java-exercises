02d6d29520cca4adb010fbd139015212eaed2c14
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-05-01 10:08:05 -0500

    data types exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,54 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Goodbye April!");
-        System.out.print("Hello May!");
+        System.out.println("Hello May!");
+
+        int myFavoriteNumber = 50;
+        System.out.println("My Favorite Number is " + myFavoriteNumber);
+
+        String myString = "It's taco night at Preston's!";
+        System.out.println(myString);
+
+//        char myString = "a";
+//        System.out.println(myString);
+//
+//        float myString = 3.14159;
+//        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//        String class = "What?"
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println(three);
+
+        int three2 = (int) 3;
+        System.out.println(three2);
+
+
+//        int x = 4;
+//        x += 5;
+
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+
     }
 }
