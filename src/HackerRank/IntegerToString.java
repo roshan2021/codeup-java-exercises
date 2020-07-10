1112b73ed2b68b34008a6ba3b6d8b0206404da7a
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-07-09 19:59:25 -0500

    integer to String, negative numbers work

--- src/HackerRank/IntegerToString.java
+++ src/HackerRank/IntegerToString.java
@@ -9,23 +9,52 @@ public class IntegerToString {
 
         try {
             Scanner in = new Scanner(System.in);
+            System.out.println("Enter an integer");
             int n = in .nextInt();
             in.close();
             //String s=???; Complete this line below
-            String s = Integer.toString(n);
             //Write your code here
-
-
+            String s = Integer.toString(n);
+            //negative numbers work
             if (n == Integer.parseInt(s)) {
                 System.out.println("Good job");
             } else {
                 System.out.println("Wrong answer.");
             }
         } catch (Exception e) {
-            System.out.println("Unsuccessful Termination!!");
+            System.out.println("Wrong answer");
         }
     }
 }
+
+
+
+//    public static void main(String[] args) {
+        //try / catch for exception if user does not input an integer
+//        try{
+//            //userInput new Scanner object
+//            Scanner userInput = new Scanner(System.in);
+//            //asking user to enter an integer
+//            System.out.println("Enter an integer");
+//            //n is assigned to the user's input of an "Integer"
+//            int n = userInput.nextInt();
+//            //convert userInput Integer "n" to a String "s"
+//            String s = Integer.toString(n);
+//            //if condition or comparision is true then print "good job"
+//            //hardcoding comparision does "n" equal the conversion of String "s" into an integer?
+//            if(n == Integer.parseInt(s)){
+//                System.out.println("Good job");
+//            }
+//            //if condition is false, print out wrong answer
+//        } catch(Exception e){
+//            System.out.println("Wrong answer");
+//        }
+//    }
+//}
+
+
+
+
 //
 ////The following class will prevent you from terminating the code using exit(0)!
 //class DoNotTerminate {
